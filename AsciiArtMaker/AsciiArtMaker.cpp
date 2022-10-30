ce609d8916ea43a4a39ae045a17e98998e25c558
Slavchoo <tothelove1@gmail.com>
2022-10-30 14:04:25 +0200

    I feel sad

---
+++ AsciiArtMaker/AsciiArtMaker.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <shobjidl.h>
+char Gradient[] = " .:-=+*/0#%@";
+void MakeArt(PWSTR path) {
+    HANDLE picture = CreateFile(path, GENERIC_READ, FILE_SHARE_READ, NULL,OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    BITMAPFILEHEADER bfh;
+    BITMAPINFOHEADER bih;
+
+    DWORD readWord;
+    std::ofstream res("File.txt");
+    if (picture == INVALID_HANDLE_VALUE) return;
+    ReadFile(picture, &bfh, sizeof(bfh), &readWord, NULL);
+    ReadFile(picture, &bih, sizeof(bih), &readWord, NULL);
+    std::cout<< "Width: " << bih.biWidth << " Height:" << bih.biHeight << std::endl;
+    char** result = new char * [bih.biWidth];
+    for (int x = 0; x < bih.biWidth; x++) {
+        result[x] = new char[bih.biHeight];
+    }
+    unsigned char pixel[4];
+    for (int y = 0; y < bih.biHeight; y++) {
+        for (int x = 0; x < bih.biWidth; x++)
+        {
+            
+            
+            ReadFile(picture, &pixel, 3, &readWord, NULL); //reading file
+            int color = pixel[0] + pixel[1] + pixel[2]; //black & white converation
+            color /= 3;
+            
+            
+            // Add contrast
+            color-=45;
+            color *= 2;
+            color = (color < 255)? color : 255;
+            
+            //result symb
+            result [x][y]=Gradient[color*11/256];
+            
+        }
+        
+        ReadFile(picture, &pixel, 1, &readWord, NULL); // ignore extra byte
+    
+    }
+    for (int y = bih.biHeight-1; y >=0; y--) {
+        for (int x = 0; x < bih.biWidth; x++)
+        {
+            //output result
+            res << result[x][y];
+
+        }
+
+        res << '\n';
+    }
+    res.close();
+
+    
+}
+int main()
+{
+    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+        COINIT_DISABLE_OLE1DDE);
+    if (SUCCEEDED(hr))
+    {
+        IFileOpenDialog* pFileOpen;
+
+        // Create the FileOpenDialog object.
+        hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+            IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+        if (SUCCEEDED(hr))
+        {
+            COMDLG_FILTERSPEC spec[]{
+                {L"BMP PHOTO", L"*.bmp"}
+            };
+            pFileOpen->SetFileTypes(1, spec);
+    
+            hr = pFileOpen->Show(NULL);
+
+            
+            if (SUCCEEDED(hr))
+            {
+                IShellItem* pItem;
+                hr = pFileOpen->GetResult(&pItem);
+                if (SUCCEEDED(hr))
+                {
+                    PWSTR pszFilePath;
+                    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+                    
+                    MakeArt(pszFilePath);
+                    if (SUCCEEDED(hr))
+                    {
+                        
+                        CoTaskMemFree(pszFilePath);
+                    }
+                    pItem->Release();
+                }
+            }
+            pFileOpen->Release();
+        }
+        CoUninitialize();
+    }
+}
